54f623f5b363694d5b0055a16da3f201ddcaa826
TheRankChamp <32547882+TheRankChamp@users.noreply.github.com>
2026-04-09 16:54:53 -0700

    Update streamlit_app.py
    adding nutrition lookup steps

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,8 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
     st.stop()
-    
+
+# New section
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
